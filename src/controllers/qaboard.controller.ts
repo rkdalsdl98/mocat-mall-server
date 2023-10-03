bc58da1b6945f2df8656a385b560e669609b34da
KangMins <rkdalsdl98@naver.com>
2023-10-03 15:27:14 +0900

    'qaboard'

--- src/controllers/qaboard.controller.ts
+++ src/controllers/qaboard.controller.ts
@@ -1,10 +1,11 @@
-import { TypedRoute } from "@nestia/core";
+import { TypedParam, TypedQuery, TypedRoute } from "@nestia/core";
 import { Controller } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { QABoardDto } from "src/dto/qaboard.dto";
+import { IQABoardQuery } from "src/query/iqaboard.query";
 import { QABoardService } from "src/services/qaboard.service";
 
-@Controller("user/qaboard")
+@Controller("qaboard")
 @ApiTags("질문글")
 export class QABoardController {
     constructor(
@@ -18,31 +19,59 @@ export class QABoardController {
      * @returns 질문글 리스트
      */
     @TypedRoute.Get()
-    async getBoards() : Promise<void> {
-        return
+    async getBoards() : Promise<QABoardDto[] | undefined> {
+        return await this.qaboardService.getQABoards()
+    }
+
+    /**
+     * 질문글 불러오기
+     * 
+     * @returns 질문글
+     */
+    @TypedRoute.Get("/:boardId")
+    async getBoardBy(
+        @TypedParam("boardId") boardId : number
+    ) : Promise<QABoardDto | null | undefined> {
+        return await this.qaboardService.getQABoardBy({ boardId })
     }
 
     /**
      * 질문글 등록
+     * 
+     * @security bearer
      */
     @TypedRoute.Post()
-    async createBoard() : Promise<void> {
-        return
+    async createBoard(
+        @TypedQuery() query: IQABoardQuery
+    ) : Promise<void> {
+        return 
     }
 
     /**
      * 질문글 수정
+     * 
+     * @security bearer
      */
     @TypedRoute.Patch()
-    async updateBoard() : Promise<void> {
-        return
+    async updateBoard(
+        @TypedQuery() query: IQABoardQuery.IQABoardQueryUpdateOtions
+    ) : Promise<void> {
+        return await this.qaboardService.updateQABoardBy({
+            title: query.title,
+            contentText: query.contentText,
+            productId: query.productId,
+        }, { boardId: query.boardId })
     }
 
     /**
      * 질문글 삭제
+     * 
+     * @security bearer
      */
     @TypedRoute.Delete()
-    async deleteBoard() : Promise<void> {
-        return
+    async deleteBoard(
+        @TypedQuery() query: IQABoardQuery
+    ) : Promise<void> {
+        return await this.qaboardService.deleteQABoard({ boardId: query.boardId })
     }
 }
